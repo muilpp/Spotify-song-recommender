429169713d00b0eb69318b34b19cbfc174730c51
Marc <muilpp@gmail.com>
2016-05-31 21:31:52 +0200

    Constants refactor

--- src/main/java/model/Constant.java
+++ src/main/java/model/Constant.java
@@ -1,8 +1,8 @@
 package model;
 
 public interface Constant {
-    public final static String CLIENT_ID = "e1422906b44f4806a5efd2af0aeeec22";
-    public final static String CLIENT_SECRET = "ff4c56c16f7a4fba919162e8cf93d851";
+    public final static String CLIENT_ID = "";
+    public final static String CLIENT_SECRET = "";
     public final static String TIME_RANGE = "time_range";
     public final static String SEED_TRACKS = "seed_tracks";
     public final static String MARKET = "market=ES";
